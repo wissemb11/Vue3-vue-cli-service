b7b785949e2d745c8e5000bcdde7672a4c88cd97
wissem <wissemb11@gmail.com>
2022-08-30 12:57:53 +0100

    .

--- src/main.js
+++ src/main.js
@@ -1,15 +1,31 @@
-import { createApp, h } from 'vue'
-import './style.css'
-// import App from './App.vue'
+import { createApp, h } from "vue";
+import "./style.css";
+
 let App = {
-    props:['props1','props2']
-    setup(props,context) {
-        return () => [h('h1', '# Vue3-COMPOSITION-API ON the SERVER SIDE'),
-            h('p', ...),
- 
-
-        ]
-
-    }
-}
-createApp(App).mount('#app')
+  props: ["props1", "props2"],
+  setup(props, context) {
+    return () => [
+      h(
+        "h1",
+        "Title1"
+      ),
+      h(
+        "p",
+        "paragraphe 1...."
+      ),
+      h(
+        "p",
+        "paragraphe 2...."
+      ),
+      h(
+        "h1",
+        "Title2"
+      ),
+       h(
+        "p",
+        "paragraphe 3...."
+      ),
+    ];
+  }
+};
+createApp(App).mount("#app");
